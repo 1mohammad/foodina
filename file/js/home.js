844c0866e3c8bb0964a0713144b2e05c1ea41a1d
mohammad karimi <jaavdanm@gmail.com>
2021-06-20 14:23:31 +0430

    new changes

--- file/js/home.js
+++ file/js/home.js
@@ -29,6 +29,23 @@ $(document).ready(() => {
         autoplay: {
             delay: 3000,
         },
+        // breakpoints: {
+        //     // when window width is >= 320px
+        //     320: {
+        //         slidesPerView: 2,
+        //         spaceBetween: 20,
+        //     },
+        //     // when window width is >= 480px
+        //     480: {
+        //         slidesPerView: 3,
+        //         spaceBetween: 30,
+        //     },
+        //     // when window width is >= 640px
+        //     640: {
+        //         slidesPerView: 4,
+        //         spaceBetween: 40,
+        //     },
+        // },
     });
     var swiper2 = new Swiper('.BestDayFood', {
         navigation: {
@@ -46,6 +63,23 @@ $(document).ready(() => {
         },
         autoplay: {
             delay: 3000,
+        },
+        breakpoints: {
+            // when window width is >= 320px
+            320: {
+                slidesPerView: 1.3,
+                spaceBetween: 20,
+            },
+            // when window width is >= 480px
+            480: {
+                slidesPerView: 1.3,
+                spaceBetween: 30,
+            },
+            // when window width is >= 640px
+            640: {
+                slidesPerView: 4,
+                spaceBetween: 40,
+            },
         },
     });
 });
